ca5abdf64beb597df4d77a30d308e35db90f49d8
Nikolai Savvinov <nsavvinv@gmail.com>
2020-06-27 10:19:31 +0100

    a few refactorings

--- ash.R
+++ ash.R
@@ -4,44 +4,44 @@ library(ggplot2)
 library(tidyr)
 library(reshape2)
 library(RColorBrewer)
-
-get_ash_data <- function(ch, category, groupby=category, alias=category, filter="1=1", weight="1", ytitle='aas', t1=format(Sys.Date()-14, "%Y-%m-%d %H:%M:%S"), t2=format(Sys.Date()+1, "%Y-%m-%d %H:%M:%S"), res='hh24', top.n.categories=10, dash=TRUE)
-{
-  print(paste0('category=', category))
-  print(paste0('groupby=', groupby))
-  print(paste0('alias=', alias))
-  print(paste0('filter=', filter))
-  print(paste0('weight=', weight))
-  
+ 
+# Generate ASH query
+get_ash_query <- function(category, groupby=category, alias=category, filter="1=1", weight="1", ytitle='aas', t1=format(Sys.Date()-14, "%Y-%m-%d %H:%M:%S"), t2=format(Sys.Date()+1, "%Y-%m-%d %H:%M:%S"), res='hh24', top.n.categories=10, dash=TRUE, nulls='N/A'){
   table <- 'GV$ACTIVE_SESSION_HISTORY'
   samples_per_min <- 60
   if(dash) {
     table <- 'DBA_HIST_ACTIVE_SESS_HISTORY'
+    filter <- gsub("inst_id", "instance_number", filter)
     if (weight==1) samples_per_min <- 6
+  }else filter <- gsub("instance_number", "inst_id", filter)
+ 
+  ashQueryText <- "with a as
+  (
+    select sample_time-to_dsinterval('0 07:00:00') t,
+    nvl(:category:, ':nulls:') :alias:,
+    sum(nvl(:weight:,0)) x
+    from :table: ash
+    where 1=1
+    and :filter:
+    and sample_time-to_dsinterval('0 07:00:00') between timestamp':t1:' and timestamp':t2:'
+    group by sample_time-to_dsinterval('0 07:00:00'), :groupby:
+    order by t, :category:
+  )
+  select trunc(t, ':res:') t, :alias:, avg(x) :ytitle:
+  from a
+  group by trunc(t, ':res:'), :alias:
+  order by 1, 2"
+ 
+  if(res=="dd") {
+    mins = 60*24
+  } else if(res=="hh24") {
+    mins = 60
+  } else {
+    mins = 1
   }
-  
-  ashQueryText <-                     "select trunc(sample_time, ':res:') t, 
-                                                 :category: :alias:, 
-  sum(:weight:)/:samples_per_min:/:mins: :ytitle: 
-  from :table: ash 
-  where 1=1
-  and :filter: 
-  and sample_time between timestamp':t1:' and timestamp':t2:'
-  group by trunc(sample_time, ':res:'), :groupby: 
-  order by trunc(sample_time, ':res:'), :category:"
-  
-#  if (weight=="1"){
-#    print(weight)
-    if(res=="dd") {
-      mins = 60*24
-    } else if(res=="hh24") {
-      mins = 60
-    } else {
-      mins = 1
-    }
-#  }
-#  else mins = 1/6
-  
+ 
+  ytitle <- gsub('[/,0-9]', '', ytitle)
+ 
   ashQueryText <- gsub(":weight:", weight, ashQueryText)
   ashQueryText <- gsub(":res:", res, ashQueryText)
   ashQueryText <- gsub(":ytitle:", ytitle, ashQueryText)
@@ -54,41 +54,55 @@ get_ash_data <- function(ch, category, groupby=category, alias=category, filter=
   ashQueryText <- gsub(":t1:", t1, ashQueryText)
   ashQueryText <- gsub(":t2:", t2, ashQueryText)
   ashQueryText <- gsub(":table:", table, ashQueryText)
-  print(ashQueryText)
+  ashQueryText <- gsub(":nulls:", nulls, ashQueryText)
+  writeLines(ashQueryText)
+  return(ashQueryText)
+}
+ 
+# Generate ASH query and get data
+get_ash_data <- function(ch, category, groupby=category, alias=category, filter="1=1", weight="1", ytitle='aas', t1=format(Sys.Date()-14, "%Y-%m-%d %H:%M:%S"), t2=format(Sys.Date()+1, "%Y-%m-%d %H:%M:%S"), res='hh24', top.n.categories=10, dash=TRUE, nulls='N/A')
+{
+  ashQueryText <- get_ash_query(category, groupby, alias, filter, weight, ytitle, t1, t2, res, top.n.categories, dash, nulls)
+  print("ashQueryText=")
+  writeLines(ashQueryText)
   q <- dbGetQuery(ch, ashQueryText)
   if(is.null(nrow(q))) stop(q)
+  print(str(q))
+  tz(q$T) <- "Europe/London"
   return(q)
 }
-
+ 
+# Build a stacked graph plot with the specified color palette using a data frame
 plot_ash_data <- function(d, basepalette='Dark2'){
   cols <- colorRampPalette(brewer.pal(8, basepalette))
   mycols <- cols(length(unique(d[,2])))
-  p = ggplot(d, aes_string(x = names(d)[[1]], y = names(d)[[3]]), label='T') + 
-    geom_area(stat='identity', aes_string(fill=names(d)[2])) + 
+  p = ggplot(d, aes_string(x = names(d)[[1]], y = names(d)[[3]]), label='T') +
+    geom_area(stat='identity', aes_string(fill=names(d)[2])) +
     scale_fill_manual(values=mycols)
-#  ggplotly(p, tooltip=text)
 }
-
-plot_ash <- function(ch, category, alias=category, groupby=category, filter="1=1", weight=1, ytitle='aas', t1=format(Sys.Date()-14, "%Y-%m-%d %H:%M:%S"), t2=format(Sys.Date()+1, "%Y-%m-%d %H:%M:%S"), res='hh24', n=5, dash=TRUE, basepalette='Dark2'){
+ 
+# Combine all functions above into one to be called from the UI
+get_tidy_and_plot_ash <- function(ch, category, alias=category, groupby=category, filter="1=1", weight=1, ytitle='aas', t1=format(Sys.Date()-14, "%Y-%m-%d %H:%M:%S"), t2=format(Sys.Date()+1, "%Y-%m-%d %H:%M:%S"), res='hh24', n=5, dash=TRUE, nulls='N/A', basepalette='Dark2'){
   print("getting data...")
-  data <- get_ash_data(ch, 
-                       category=category, 
-                       groupby=groupby, 
-                       alias=alias, 
-                       filter=filter, 
-                       weight=weight, 
-                       ytitle=ytitle, 
-                       t1=t1, 
-                       t2=t2, 
-                       res=res, 
-                       dash=dash)
+  data <- get_ash_data(ch,
+                       category=category,
+                       groupby=groupby,
+                       alias=alias,
+                       filter=filter,
+                       weight=weight,
+                       ytitle=ytitle,
+                       t1=t1,
+                       t2=t2,
+                       res=res,
+                       dash=dash,
+                       nulls=nulls)
   oldnames <- names(data)
 #  print(head(data))
   print("gott the data")
   data[,2] <- myfactor(data[,2], data[,3], n)
   data <- aggregate(data[,3], by=list(data[,1], data[,2]), FUN=sum)
   names(data) <- oldnames
-  
+ 
   data[,2] <- reorder(data[,2], data[,3])
   data <- spread_(data, key=oldnames[[2]], value=oldnames[[3]], fill=0)
   time_unit = 'day'
@@ -109,12 +123,14 @@ plot_ash <- function(ch, category, alias=category, groupby=category, filter="1=1
   return(p)
 #  return(data)
 }
-  
+ 
+#not being used
 hotfactor= function(fac,by,n=10,o="other") {
   levels(fac)[rank(-xtabs(by~fac))[levels(fac)]>n] <- o
   fac
 }
-
+ 
+# replace all factors except top n most popular with 'other'
 myfactor <- function(fac, val, n){
   n <- as.numeric(n)
   fac <- as.character(fac)
--- server.R
+++ server.R
@@ -2,71 +2,110 @@ source("./ash.R")
 #library(RODBC)
 library(ROracle)
 library(lubridate)
-
-ch <- reactiveValues()
-ch <- dbConnect(dbDriver("Oracle"), username="", password="", dbname="")
-
-
+ 
+global <- reactiveValues()
+ 
+ 
+ 
 getColumns <- function(ch){
   dbGetQuery(ch, "select column_name from dba_tab_columns where table_name = 'DBA_HIST_ACTIVE_SESS_HISTORY' order by column_name")
 }
-
+ 
 function(input, output, session){
-  updateSelectizeInput(session, "category", choices=getColumns(ch), selected='WAIT_CLASS')
   updateTextInput(session, 'categoryExpr', value="NVL(WAIT_CLASS, 'ON CPU')")
+ 
+  output$connectStatus <- renderText({
+    if(!is.null(global$ch)){
+      print(global$ch)
+      x <- dbGetQuery(global$ch, "select global_name from global_name")
+      str(x)
+      return(paste("Connected to: ", x$GLOBAL_NAME))
+    }else{
+      #             shinyjs::hide(id="txtTest")
+      return(paste("not connected:", global$connection_error))
+    }
+  })
+  observeEvent(input$btnConnect, {
+    tryCatch(
+      {
+        global$ch <- dbConnect(dbDriver("Oracle"), username=input$txtUsername, password=input$pwdDatabase, dbname=input$txtDatabase)  
+        global$connected <- "yes"
+        shinyjs::show(id="tabs")
+        updateSelectizeInput(session, "category", choices=getColumns(global$ch), selected='WAIT_CLASS')
+       
+      },
+      error = function(e){
+        global$connected <- "no"
+        global$connection_error <- e
+        shinyjs::hide(id="tabs")
+        global$ch <- NULL
+        print("set global$ch to NULL")
+      }
+    )
+     
+  })
   
   observe({
     input$category
     updateTextInput(session, "categoryExpr", value=input$category)
   })
-  
+ 
   isolate(input$topN)
-  
+ 
   observeEvent(input$btnPlot, {
     input$topN
-    input$startDate
-    input$endDate
-    input$startTime
-    input$endTime
+    isolate(input$startDate)
+    isolate(input$endDate)
+    isolate(input$startTime)
+    isolate(input$endTime)
     isolate(input$categoryExpr)
-    
+   
     output$ashPlot <- renderPlotly({
+      isolate(input$startDate)
+      isolate(input$endDate)
+      isolate(input$startTime)
+      isolate(input$endTime)
+      isolate(input$categoryExpr)
+     
       ytitle=input$weight
       if(ytitle=="1"){ytitle<-"average_active_sessions"}
       print(paste0("ytitle=", ytitle))
       t1=switch(input$radPlotRange,
-                'month' = format(Sys.Date()-31, "%Y-%m-%d %H:%M:%S"),
-                'week' =  format(Sys.Date()-7,  "%Y-%m-%d %H:%M:%S"),
-                'day'=   format(Sys.Date()-1, "%Y-%m-%d %H:%M:%S"),
+                'month' = format(Sys.time()-days(31), "%Y-%m-%d %H:%M:%S"),
+                'week' =  format(Sys.time()-days(7),  "%Y-%m-%d %H:%M:%S"),
+                'day'=   format(Sys.time()-days(1), "%Y-%m-%d %H:%M:%S"),
                 'hour' = format(Sys.time()-hours(1), "%Y-%m-%d %H:%M:%S"),
                 'all' = format(Sys.time()-days(1000), "%Y-%m-%d %H:%M:%S"),
-                'custom'=paste0(input$startDate, ' ', input$startTime, ':00'),
+                'custom'=paste0(isolate(input$startDate), ' ', isolate(input$startTime), ':00'),
                 stop('wrong t1 format!')
-                                                
+                                               
       )
       t2=switch(input$radPlotRange,
-                'custom'=paste0(input$endDate, ' ', input$endTime, ':00'),
+                'custom'=paste0(isolate(input$endDate), ' ', isolate(input$endTime), ':00'),
                 format(Sys.time()+minutes(30), "%Y-%m-%d %H:%M:%S")
                 )
       withProgress(
-        plot_ash(ch, 
-                 category=isolate(input$categoryExpr), 
-                 groupby=isolate(input$category), 
-                 alias='legend', 
-                 filter=isolate(ifelse(input$addlFilter=='',"1=1", input$addlFilter)), 
-                 weight=input$weight, 
-                 ytitle=ytitle,
-                 t1=t1, 
-                 t2=t2, 
-                 res=input$resolution, 
-                 n=isolate(input$topN),
-                 dash=input$chkIncludeDash,
-                 basepalette=input$palette
+        tryCatch(
+          get_tidy_and_plot_ash(global$ch,
+                   category=isolate(input$categoryExpr),
+                   groupby=isolate(input$category),
+                   alias='legend',
+                   filter=isolate(ifelse(input$addlFilter=='',"1=1", input$addlFilter)),
+                   weight=input$weight,
+                   ytitle=ytitle,
+                   t1=t1,
+                   t2=t2,
+                   res=input$resolution,
+                   n=isolate(input$topN),
+                   dash=input$chkIncludeDash,
+                   basepalette=input$palette
+          ), error=function(e) stop(safeError(e))
         ),
         message='Plotting...'
       )
     })
-    
+   
   })
-  
+ 
 }
+ 
